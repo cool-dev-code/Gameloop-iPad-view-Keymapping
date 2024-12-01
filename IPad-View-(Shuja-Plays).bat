4cbd1fda34fbde19adc8cdacc77fa1ac0b770b0b
Shuja Ahmed <65089197+cool-dev-code@users.noreply.github.com>
2024-12-01 18:22:26 +0500

    Update IPad-View-(Shuja-Plays).bat

--- IPad-View-(Shuja-Plays).bat
+++ IPad-View-(Shuja-Plays).bat
@@ -8,7 +8,8 @@ echo    ^|      iPad view by Shuja Plays        ^|
 echo    ^|                                      ^|
 echo    ^|          (1) Get iPad View           ^|
 echo    ^|          (2) Remove iPad View        ^|
-echo    ^|          (3) Exit                    ^|
+echo    ^|          (3) Goto my GitHub          ^|
+echo    ^|          (4) Exit                    ^|
 echo    ^|                                      ^|
 echo    ^|______________________________________^|
 echo.
@@ -16,44 +17,113 @@ echo.
 
 set /p choice=Enter your choice 1,2 or 3: 
 
-if "%choice%"=="1" goto download_replace
-if "%choice%"=="2" goto delete_file
-if "%choice%"=="3" goto exit_program
+if "%choice%"=="1" goto download_ipad_view
+if "%choice%"=="2" goto remove_ipad_view
+if "%choice%"=="3" goto github
+if "%choice%"=="4" goto exit
 
 echo.
 echo Invalid choice. Please try again.
 pause
 goto menu
 
-:download_replace
+:download_ipad_view
+mkdir "%APPDATA%\AndroidTbox"
+cls
+echo ALERT !!! Make sure to check that iPad view is upto date on my GitHub before continuing...
+
+echo     _______________________________________
+echo    ^|                                      ^|
+echo    ^|          (1) Contine                 ^|
+echo    ^|          (2) Goto my GitHub          ^|
+echo    ^|          (3) Back to Main Menu       ^|
+echo    ^|______________________________________^|
 echo.
-echo Downloading Latest Keymapping file...
+
+
+set /p choice=Enter your choice: 
+
+if "%choice%"=="1" (echo.)
+if "%choice%"=="2" goto github
+if "%choice%"=="3" goto menu
+cls
+echo   _____________________________________________
+echo  ^|                                            ^|
+echo  ^|   Creating backup of Keymapping file...    ^|
+echo  ^|____________________________________________^|
+echo.
+set "localFile=%APPDATA%\AndroidTbox\TVM_100.xml"
+set "backupFile=%APPDATA%\AndroidTbox\TVM_100.xml.bak"
+
+if exist "%localFile%" (
+copy "%localFile%" "%backupFile%"
+echo   ___________________________________
+echo  ^|                                  ^|
+echo  ^|   Backup created successfully    ^|
+echo  ^|__________________________________^|
+echo.
+echo Backup Location is %backupFile%
+
+) else (
+echo   ___________________________________________________________
+echo  ^|                                                          ^|
+echo  ^|   No existing Keymapping file found, skipping backup.    ^|
+echo  ^|__________________________________________________________^|
+)
+
+echo.
+echo   _____________________________________________
+echo  ^|                                            ^|
+echo  ^|   Downloading Latest Keymapping file...    ^|
+echo  ^|____________________________________________^|
 echo.
 curl -o "%APPDATA%\AndroidTbox\TVM_100.xml" "https://raw.githubusercontent.com/cool-dev-code/Gameloop-iPad-view-Keymapping/main/TVM_100.xml"
-echo iPad view applied successfully.
+echo   _______________________________________
+echo  ^|                                      ^|
+echo  ^|   iPad view applied successfully.    ^|
+echo  ^|______________________________________^|
 echo.
 echo   ______________________________________
+echo  ^|                                     ^|
 echo  ^|       Subscribe to my Channel       ^|
 echo  ^|_____________________________________^|
 pause
 start "Subscribe" "https://www.youtube.com/@ShujaPlays"
 goto menu
 
-:delete_file
-echo.
+:remove_ipad_view
+cls
 setlocal
 
 set "localFile=%APPDATA%\AndroidTbox\TVM_100.xml"
 
 if exist "%localFile%" (
     del "%localFile%"
-    echo iPad view removed successfully.) else (
-    echo iPad view was not found.
-	echo.
+	echo   __________________________________________
+	echo  ^|                                         ^|
+	echo  ^|     iPad view removed successfully.     ^|
+	echo  ^|_________________________________________^|) else (
+    echo   __________________________________________
+	echo  ^|                                         ^|
+	echo  ^|     iPad view was not found.            ^|
+	echo  ^|_________________________________________^|)
 )
-
+echo.
 pause
 goto menu
 
-:exit_program
+
+:github
+start "GitHub" "https://github.com/cool-dev-code/Gameloop-iPad-view-Keymapping"
+    echo   __________________________________________
+	echo  ^|                                         ^|
+	echo  ^|     Opened my GitHub in Background      ^|
+	echo  ^|_________________________________________^|
+timeout /t 1 >nul
+goto menu
+
+:exit
+cls
+echo Bye Bye
+timeout /t 1 >nul
 exit
